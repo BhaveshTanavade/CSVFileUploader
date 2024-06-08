b182ebb03bddbbe60e29d1cf6ed78c8975933873
BhaveshTanavade <bhavtanavade1@gmail.com>
2024-06-08 12:32:36 +0530

    update app.js

--- app.js
+++ app.js
@@ -41,6 +41,10 @@ app.get('/file/:filename', (req, res) => {
     });
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
